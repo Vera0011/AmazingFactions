9fa125db9e2bffc059881b6dfd4e54791241d52c
Vera <contact.vera0011@gmail.com>
2024-12-30 01:51:08 +0100

    Added Faction DAO

--- src/main/java/org/vera/amazingFactions/daos/FactionDAO.java
+++ src/main/java/org/vera/amazingFactions/daos/FactionDAO.java
@@ -1,4 +1,13 @@
 package org.vera.amazingFactions.daos;
 
+import org.vera.amazingFactions.types.Faction;
+
+import java.sql.Connection;
+
 public class FactionDAO {
+    private Connection connection;
+
+    public void createFaction(Faction faction) {}
+    public void getFactionByLeader(String leaderId) {}
+    public void getFactionById(String factionId) {}
 }

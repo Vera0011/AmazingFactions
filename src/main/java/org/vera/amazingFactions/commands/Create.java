3082fa512f6283cb21f114b9c1cb05877cc8b7c7
Vera <contact.vera0011@gmail.com>
2024-12-30 19:44:54 +0100

    Added throws to DTO and DAO methods

--- src/main/java/org/vera/amazingFactions/commands/Create.java
+++ src/main/java/org/vera/amazingFactions/commands/Create.java
@@ -14,49 +14,46 @@ public class Create implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player currentPlayer) {
-            if (args.length < 2) {
-                currentPlayer.sendMessage(ChatColor.RED + "[AmazingFactions]" + ChatColor.GOLD + " - Usage: /create <name> <description> <list of the users>");
-                return true;
-            }
+        Player currentPlayer = (Player) sender;
 
-            String name = args[0];
-            String description = args[1];
-            Set<UUID> users = new HashSet<>();
-            UUID leaderUUID = currentPlayer.getUniqueId();
-
-            // Adds multiple users, or only one (the leader, executor of the command)
-            if (args.length > 2) {
-                String leader = currentPlayer.getName();
-                String[] userArgs = Arrays.copyOfRange(args, 2, args.length);
-                userArgs[userArgs.length - 1] = leader;
-
-                // Convert the array to a HashSet to remove duplicates
-                Set<String> usersInput = new HashSet<>(Arrays.asList(userArgs));
-
-                for (String user : usersInput) {
-                    Player validUser = Bukkit.getPlayer(user);
-
-                    if (validUser != null) {
-                        users.add(validUser.getUniqueId());
-                    }
-                }
-            } else {
-                users.add(leaderUUID);
-            }
-
-            FactionService.createFaction(name, description, users, leaderUUID);
-
-            currentPlayer.sendMessage(ChatColor.RED + "[AmazingFactions]" + ChatColor.GOLD + " - You created a new faction." +
-                    ChatColor.AQUA + "\n|| -->  Name: " + ChatColor.GREEN + name +
-                    ChatColor.AQUA + "\n|| -->  Description: " + ChatColor.GREEN + description +
-                    ChatColor.AQUA + "\n|| -->  Leader: " + ChatColor.GREEN + currentPlayer.getName() +
-                    ChatColor.AQUA + "\n|| -->  Number of users: " + ChatColor.GREEN + users.size());
-
+        if (args.length < 2) {
+            currentPlayer.sendMessage(ChatColor.RED + "[AmazingFactions]" + ChatColor.GOLD + " - Usage: /create <name> <description> <list of the users>");
             return true;
         }
 
-        System.out.println("This command can only be executed by a player");
+        String name = args[0];
+        String description = args[1];
+        Set<UUID> users = new HashSet<>();
+        UUID leaderUUID = currentPlayer.getUniqueId();
+
+        // Adds multiple users, or only one (the leader, executor of the command)
+        if (args.length > 2) {
+            String leader = currentPlayer.getName();
+            String[] userArgs = Arrays.copyOfRange(args, 2, args.length);
+            userArgs[userArgs.length - 1] = leader;
+
+            // Convert the array to a HashSet to remove duplicates
+            Set<String> usersInput = new HashSet<>(Arrays.asList(userArgs));
+
+            for (String user : usersInput) {
+                Player validUser = Bukkit.getPlayer(user);
+
+                if (validUser != null) {
+                    users.add(validUser.getUniqueId());
+                }
+            }
+        } else {
+            users.add(leaderUUID);
+        }
+
+        //FactionService.createFaction(name, description, users, leaderUUID);
+
+        currentPlayer.sendMessage(ChatColor.RED + "[AmazingFactions]" + ChatColor.GOLD + " - You created a new faction." +
+                ChatColor.AQUA + "\n|| -->  Name: " + ChatColor.GREEN + name +
+                ChatColor.AQUA + "\n|| -->  Description: " + ChatColor.GREEN + description +
+                ChatColor.AQUA + "\n|| -->  Leader: " + ChatColor.GREEN + currentPlayer.getName() +
+                ChatColor.AQUA + "\n|| -->  Number of users: " + ChatColor.GREEN + users.size());
+
         return true;
     }
 }
--- src/main/java/org/vera/amazingFactions/services/FactionService.java
+++ src/main/java/org/vera/amazingFactions/services/FactionService.java
@@ -3,13 +3,14 @@ package org.vera.amazingFactions.services;
 import org.vera.amazingFactions.dao.FactionDAO;
 import org.vera.amazingFactions.dto.FactionDTO;
 
+import java.sql.SQLException;
 import java.util.Set;
 import java.util.UUID;
 
 public class FactionService {
     private static final FactionDAO factionDAO = new FactionDAO();
 
-    public static void createFaction(String name, String description, Set<UUID> users, UUID leader) {
+    public static void createFaction(String name, String description, Set<UUID> users, UUID leader) throws SQLException {
         FactionDTO newFaction = new FactionDTO(name, description, users, leader);
         factionDAO.createFaction(newFaction);
     }

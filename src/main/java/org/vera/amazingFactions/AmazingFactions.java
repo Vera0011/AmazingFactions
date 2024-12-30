0d65ee899f605d52b534dfe301201c45ec8941d6
Vera <contact.vera0011@gmail.com>
2024-12-30 20:25:01 +0100

    Modified DatabaseConnector logic to apply Singleton

--- src/main/java/org/vera/amazingFactions/AmazingFactions.java
+++ src/main/java/org/vera/amazingFactions/AmazingFactions.java
@@ -4,11 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.vera.amazingFactions.commands.Create;
 import org.vera.amazingFactions.internal.DatabaseConnector;
 
-import java.sql.SQLException;
 import java.util.Objects;
 
 public final class AmazingFactions extends JavaPlugin {
-    DatabaseConnector database;
+    private DatabaseConnector database;
 
     @Override
     public void onEnable() {
@@ -40,30 +39,14 @@ public final class AmazingFactions extends JavaPlugin {
      */
     private boolean databaseConnect() {
         database = new DatabaseConnector("AmazingFactions");
-
-        try {
-            database.connect();
-
-            return database.executeInitialization();
-        } catch (SQLException e) {
-            getLogger().severe("Error while connecting to the database: " + e.getMessage());
-            getLogger().severe("Amazing Factions could not be loaded.");
-            getServer().getPluginManager().disablePlugin(this);
-
-            return false;
-        }
+        database.connect();
+        return database.executeInitialization();
     }
 
     /**
      * Method to stop a database connection
      */
     private void databaseDisconnect() {
-        try {
-            if (database != null) {
-                database.disconnect();
-            }
-        } catch (SQLException e) {
-            getLogger().severe("Error closing the database connection: " + e.getMessage());
-        }
+        database.disconnect();
     }
 }
--- src/main/java/org/vera/amazingFactions/internal/DatabaseConnector.java
+++ src/main/java/org/vera/amazingFactions/internal/DatabaseConnector.java
@@ -1,48 +1,78 @@
 package org.vera.amazingFactions.internal;
 
+import org.vera.amazingFactions.AmazingFactions;
+
 import java.io.*;
 import java.sql.*;
 
 import static org.bukkit.Bukkit.getLogger;
+import static org.bukkit.Bukkit.getServer;
 
 /**
  * This class manages the Database connection (SQLite)
+ * Uses Singleton pattern to avoid problems
  */
 public class DatabaseConnector {
-    private Connection connection;
+    private static Connection connection;
     private final String databaseName;
 
     public DatabaseConnector(String databaseName) {
         this.databaseName = databaseName;
+        connection = null;
     }
 
-    public void connect() throws SQLException {
-        File dataFolder = new File("plugins/AmazingFactions/" + databaseName + ".db");
+    /**
+     * Creates a connection to the database
+     */
+    public void connect() {
+        if (connection == null) {
+            File dataFolder = new File("plugins/AmazingFactions/" + databaseName + ".db");
 
-        if (!dataFolder.exists()) {
-            dataFolder.getParentFile().mkdirs();
+            if (!dataFolder.exists()) {
+                dataFolder.getParentFile().mkdirs();
+            }
+
+            String url = "jdbc:sqlite:" + dataFolder.getAbsolutePath();
+            try {
+                connection = DriverManager.getConnection(url);
+            } catch (SQLException e) {
+                getLogger().severe("Error while connecting to the database: " + e.getMessage());
+                getLogger().severe("Amazing Factions could not be loaded.");
+                getServer().getPluginManager().disablePlugin(new AmazingFactions());
+            }
         }
-
-        String url = "jdbc:sqlite:" + dataFolder.getAbsolutePath();
-        connection = DriverManager.getConnection(url);
     }
 
-    public Connection getConnection() {
+    /**
+     * Returns the actual connection
+     *
+     * @return
+     */
+    public static Connection getConnection() {
         return connection;
     }
 
-    public void disconnect() throws SQLException {
-        if (connection != null && !connection.isClosed()) {
-            connection.close();
+    /**
+     * Disconnects the connection from the database
+     */
+    public void disconnect() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                connection = null;
+            }
+        } catch (SQLException e) {
+            getLogger().severe("Error closing the database connection: " + e.getMessage());
         }
     }
 
     /**
      * Executes the database creation (tables, foreign keys...)
+     *
      * @return
      */
     public boolean executeInitialization() {
-        try (Connection conn = this.connection) {
+        try (Connection conn = connection) {
             Statement stmt = conn.createStatement();
             InputStream inputStream = getClass().getClassLoader().getResourceAsStream("sql/initialization.sql");
             try {
